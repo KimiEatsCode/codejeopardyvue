044b8c31e4c28662d72538d25a81fb9a09841caa
Kimi Rettig <sarahkimirettig@gmail.com>
2023-12-12 08:57:49 -0500

    Comment out firebase for now since using render instead for cloud db

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./store";
+// import firebase from "@/helpers/firebase";
+// import firebaseConfig from "@/config/firebase";
 
-createApp(App).use(store).mount('#app');
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+createApp(App).use(store).mount("#app");
