09811bb0a89f7b4d5d8be677d1dabdde40cc1cb9
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-20 11:53:45 -0500

    3000

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,7 @@ export default createStore({
       score: 0,
       value: "",
       answeredCorrect: null,
-      url: "https://codejeopardy-api-service-ap1e.onrender.com",
+      url: "https://codejeopardy-api-service-ap1e:3000",
       //use localhost3000 url for development and production when hosting client and api on render.com host
       // url: "http://localhost:3000",
       getResponse: true,
