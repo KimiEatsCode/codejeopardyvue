b232449ab996277279c906ba6d862109a940fa9f
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-23 20:50:21 -0500

    cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+const cors = require('cors')
 
 const app = express();
-
+app.use(cors())
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 8080;
