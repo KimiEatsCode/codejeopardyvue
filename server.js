d8db9fdbfc1d3c880254def8c042eeee39296a60
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-28 09:25:36 -0500

    comment out cors

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-const cors = require('cors')
-
+// const cors = require('cors')
 const app = express();
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
-app.use(cors());
+// app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 const port = process.env.PORT || 8080;
 app.listen(port);
