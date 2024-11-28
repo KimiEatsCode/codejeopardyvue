55074587cdda1b3c89da51eec207d4b172d7e6f4
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-28 09:40:45 -0500

    remove cors code on frontend

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-const cors = require('cors')
+// const cors = require('cors')
 const app = express();
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
-app.use(cors());
+// app.use(cors());
+
+
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
