7121e6a78a6bd3d6d337667c65630f33b5d59c6c
Kimi Rettig <sarahkimirettig@gmail.com>
2024-11-27 21:20:40 -0500

    sss

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const path = require('path');
 const cors = require('cors')
 
 const app = express();
-app.use(cors())
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port);
