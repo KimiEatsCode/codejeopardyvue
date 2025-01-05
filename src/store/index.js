708df552afea8b98a9f94ab647e736dff59cfcfd
Kimi Rettig <sarahkimirettig@gmail.com>
2025-01-05 11:35:18 -0500

    Import fix fetchUserGameInfo needed payload object for multiple params

--- src/store/index.js
+++ src/store/index.js
@@ -8,12 +8,15 @@ const apiClient = axios.create({
 export default createStore({
   state() {
     return {
-      users:"",
-      userGames:"",
+      users: "",
+      userid: "",
+      playername: "",
+      userGames: "",
+      userGameInfo: "",
       categories: "",
       gameid: "",
       gameScore: "",
-      gameName:"",
+      gameName: "",
       clue: "",
       clueText: "",
       question: "",
@@ -25,7 +28,7 @@ export default createStore({
       score: "",
       clueValue: "",
       answeredCorrect: null,
-    //  url: "https://codejeo-7137663a4c65.herokuapp.com",
+      //  url: "https://codejeo-7137663a4c65.herokuapp.com",
       url: "http://localhost:3000",
       getResponse: true,
     };
@@ -55,18 +58,19 @@ export default createStore({
       apiClient
         .get(`${this.state.url}/api/gameslist/${userid}`)
         .then((res) => {
-          console.log("all games for user from fetchUserGames  " + JSON.stringify(res.data));
-          commit("setUserGames", res.data);
+         commit("setUserGames", res.data);
         })
         .catch((error) => {
           console.log(error + " fetch all user games error");
         });
     },
-    async fetchGameInfo({ commit }, gameid ) {
+    async fetchGameInfo({ commit }, gameid) {
       apiClient
         .get(`${this.state.url}/api/games/${gameid}`)
         .then((res) => {
-          console.log("fetchGameInfo call in store " + JSON.stringify(res.data));
+          console.log(
+            "fetchGameInfo call in store " + JSON.stringify(res.data)
+          );
 
           commit("setGameInfo", res.data);
         })
@@ -74,7 +78,39 @@ export default createStore({
           console.log(error + " fetch game info error");
         });
     },
+    //wrong below because can't pass params this way need to put
+    //params in an object and parse it out in the api call
 
+    // async fetchUserGameInfo({ commit }, gameid, userid) {
+    //   apiClient
+    //     .get(`${this.state.url}/api/gameslist/${gameid}/${userid}`)
+    //     .then((res) => {
+    //       console.log(
+    //         "fetch User GameInfo call in store " + JSON.stringify(res.data)
+    //       );
+
+    //       commit("setUserGame", res.data);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error + " fetch game for a user info error");
+    //     });
+    // },
+    async fetchUserGameInfo({ commit }, payload) {
+      apiClient
+        .get(
+          `${this.state.url}/api/gameslist/${payload.userid}/${payload.gameid}`
+        )
+        .then((res) => {
+          console.log(
+            "fetch User GameInfo call in store " + JSON.stringify(res.data)
+          );
+
+          commit("setUserGame", res.data);
+        })
+        .catch((error) => {
+          console.log(error + " fetch game for a user info error");
+        });
+    },
     async fetchAllCat({ commit }, gameid) {
       apiClient
         .get(`${this.state.url}/api/games/${gameid}/categories`)
@@ -144,7 +180,9 @@ export default createStore({
       apiClient
         .patch(`${this.state.url}/api/games/${payload.gameid}/${payload.score}`)
         .then(() => {
-          console.log("set score patch action " + JSON.stringify(payload.gameid));
+          console.log(
+            "set score patch action " + JSON.stringify(payload.gameid)
+          );
           // console.log("set score patch action " + JSON.stringify(res));
           commit("setScore", payload.score);
         })
@@ -155,29 +193,49 @@ export default createStore({
   },
   mutations: {
     setAllUsers(state, usersInfo) {
-      state.users = usersInfo
-      console.log("setAllUsers mutation in store " + JSON.stringify(state.users))
+      state.users = usersInfo;
+      console.log(
+        "setAllUsers mutation in store " + JSON.stringify(state.users)
+      );
       return state.users;
-      },
-    // setAllGames(state, allGamesInfo) {
-    // state.games = allGamesInfo
-    // console.log("setAllGames mutation in store " + JSON.stringify(state.games))
-    // return state.games;
-    // },
+    },
+    setAllGames(state, allGamesInfo) {
+      state.games = allGamesInfo;
+      console.log(
+        "setAllGames mutation in store " + JSON.stringify(state.games)
+      );
+      return state.games;
+    },
     setUserGames(state, allGamesInfo) {
-      state.userGames = allGamesInfo
-      console.log("setAllGames mutation in store " + JSON.stringify(state.userGames))
+      (state.userGames = allGamesInfo),
+        (state.userid = allGamesInfo[0].userid_games);
+      state.username = allGamesInfo[0].username;
+      console.log(
+        "setUserGames for user mutation in store " +
+          JSON.stringify(allGamesInfo[0].userid_games)
+      );
       return state.userGames;
-      },
-    setGameInfo(state, gameInfo) {
-      console.log("game info score " + JSON.stringify(gameInfo))
-      state.game = gameInfo
-      state.gameid = gameInfo['game_id'];
-      state.gameScore = gameInfo['game_score'];
-      console.log("setGameInfo mutation in store " + state.gameid + " and " + state.gameScore)
-      state.gameName = gameInfo['game_name'];
-      return gameInfo;
-      },
+    },
+
+    setUserGame(state, userGameInfo) {
+      state.userGameInfo = userGameInfo[0];
+      state.userid = userGameInfo[0].userid_games;
+      console.log(
+        "set User game info for user mutation in store " +
+          JSON.stringify(state.userGameInfo)
+      );
+      return state.userGameInfo;
+    },
+
+    // setGameInfo(state, gameInfo) {
+    //   console.log("game info score " + JSON.stringify(gameInfo))
+    //   state.game = gameInfo
+    //   state.gameid = gameInfo['game_id'];
+    //   state.gameScore = gameInfo['game_score'];
+    //   console.log("setGameInfo mutation in store " + state.gameid + " and " + state.gameScore)
+    //   state.gameName = gameInfo['game_name'];
+    //   return state.gameInfo;
+    //   },
 
     fetchCategories(state, payload) {
       state.categories = payload;
@@ -200,7 +258,7 @@ export default createStore({
       console.log("From mutation clues ", payload);
       state.clues = payload;
       state.getResponse = true;
-      return state.clues
+      return state.clues;
     },
     setClue(state, clue) {
       console.log("set clue in index file " + JSON.stringify(clue));
