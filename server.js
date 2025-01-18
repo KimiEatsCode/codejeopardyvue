da64c0dabf6c938d1d50c4d632b63f23fe9c52fa
Kimi Rettig <sarahkimirettig@gmail.com>
2025-01-18 12:58:59 -0500

    server js update to fix route history for hosted spa

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(
   })
 );
 app.use(cors({ origin: true })); // enable origin cors
+
 app.use(serveStatic(path.join(__dirname, "dist")));
+
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
 app.options("*", cors());
 app.use(
   cors({
