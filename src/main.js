c07ac71c030af3d9efbd635d455a207a6d7d4620
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2025-09-08 15:51:54 -0400

    Moved style css import above bootstrap css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import store from "./store";
 import router from "./router/index";
 
