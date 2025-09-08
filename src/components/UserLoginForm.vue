d0a96793ae4923709e8faaf29ee69820dcab4d11
Sarah Kimi Rettig <sarahkimirettig@gmail.com>
2025-09-08 13:37:44 -0400

    Updated login to temp link to user 1 game list

--- src/components/UserLoginForm.vue
+++ src/components/UserLoginForm.vue
@@ -2,15 +2,16 @@
   <div>
     <h2>User Login</h2>
     <form @submit.prevent="login">
-      <div>
+      
+      <!-- <div>
         <label for="username">Username:</label>
         <input type="text" id="username" v-model="username" required>
-      </div>
-      <div>
+      </div> -->
+      <!-- <div>
         <label for="password">Password:</label>
         <input type="password" id="password" v-model="password" required>
-      </div>
-      <button type="submit">Login</button>
+      </div> -->
+      <a href="/gameslist/1"><button type="submit">View Games</button></a>
       <div v-if="error" class="error">{{ error }}</div>
     </form>
   </div>
